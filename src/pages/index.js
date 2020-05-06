813967ad757490dbdabcc67c13a94165913333ab
masayaShinoda <masayashida@gmail.com>
2020-05-07 00:01:46 +0700

    added BlogPage component into index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Helmet from "react-helmet"
 import Layout from "../components/layout"
-import blogPage from "../components/blogPage"
+import BlogPage from "../components/blogPage"
 
 //bootstrap import
 import "bootstrap/dist/css/bootstrap.css"
@@ -19,7 +19,7 @@ const IndexPage = () => {
           content="Welcome to the student run newspaper covering events in Western International School."
         />
       </Helmet>
-      <blogPage></blogPage>
+      <BlogPage></BlogPage>
     </Layout>
   )
 }

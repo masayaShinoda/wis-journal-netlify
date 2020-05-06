d9208e4b9289c3e7bbf653a5c46a34f5b69591b0
masayaShinoda <masayashida@gmail.com>
2020-05-07 00:18:38 +0700

    added blogPage to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
 import Helmet from "react-helmet"
 import Layout from "../components/layout"
-import BlogPage from "../components/blogPage"
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
 
 //bootstrap import
 import "bootstrap/dist/css/bootstrap.css"
@@ -9,7 +10,15 @@ import "bootstrap/dist/css/bootstrap.css"
 //styles import
 import "../styles/styles.css"
 
-const IndexPage = () => {
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+
   return (
     <Layout>
       <Helmet>
@@ -19,9 +28,27 @@ const IndexPage = () => {
           content="Welcome to the student run newspaper covering events in Western International School."
         />
       </Helmet>
-      <BlogPage></BlogPage>
+      <div>{Posts}</div>
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`

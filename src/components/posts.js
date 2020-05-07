fd096d9dd4d86a2de8d1b0c0868efa5f2a3c5a8f
masayaShinoda <masayashida@gmail.com>
2020-05-07 21:34:00 +0700

    added posts.js

---
+++ src/components/posts.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
+
+//bootstrap import
+import "bootstrap/dist/css/bootstrap.css"
+
+const Posts = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+
+  return <div className="postsContainer">{Posts}</div>
+}
+
+export default Posts
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`

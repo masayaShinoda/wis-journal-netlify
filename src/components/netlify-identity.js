f9d70bf551f49e0a7eab2ef42e0b8a4ca292e7ed
masayaShinoda <masayashida@gmail.com>
2020-05-20 14:50:47 +0700

    added netlifyidentitywidget back

--- src/components/netlify-identity.js
+++ src/components/netlify-identity.js
@@ -1,16 +1,49 @@
 import React from "react"
-//styles import
+import IdentityModal, {
+  useIdentityContext,
+} from "react-netlify-identity-widget"
+import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
 import styles from "./Component.module.css"
 
-const NetlifyIdentity = () => {
+const NetlifyIdentity = ({ children }) => {
+  const identity = useIdentityContext() // see https://github.com/sw-yx/react-netlify-identity for api of this identity object
+  const [dialog, setDialog] = React.useState(false)
+  const name =
+    (identity &&
+      identity.user &&
+      identity.user.user_metadata &&
+      identity.user.user_metadata.name) ||
+    "NoName"
+
+  console.log(JSON.stringify(identity))
+  const isLoggedIn = identity && identity.isLoggedIn
   return (
-    <div className={styles.netlifyIdentity}>
-      <a className="btn" href="/admin">
-        <i className="fa fa-sign-in" style={{ marginRight: `0.5em` }}></i>
-        <p>Log in</p>
-      </a>
-    </div>
+    <>
+      <nav style={{ background: "green" }}>
+        {" "}
+        Login Status:
+        <button className="btn" onClick={() => setDialog(true)}>
+          {isLoggedIn ? `Hello ${name}, Log out here!` : "LOG IN"}
+        </button>
+      </nav>
+      <main>{children}</main>
+      <IdentityModal
+        showDialog={dialog}
+        onCloseDialog={() => setDialog(false)}
+      />
+    </>
   )
 }
+
+// const NetlifyIdentity = () => {
+//   return (
+//     <div className={styles.netlifyIdentity}>
+//       <a className="btn" href="/admin">
+//         <i className="fa fa-sign-in" style={{ marginRight: `0.5em` }}></i>
+//         <p>Log in</p>
+//       </a>
+//     </div>
+//   )
+// }
 
 export default NetlifyIdentity
